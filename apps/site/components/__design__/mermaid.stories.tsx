29d30ff76d86c8c5835ec4950e8b50116bc1184a
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2025-03-10 22:26:18 +0100

    WIP

---
+++ apps/site/components/__design__/mermaid.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta as MetaObj, StoryObj } from '@storybook/react';
+import { VFile } from 'vfile';
+
+import { compile } from '@/next.mdx.compiler.mjs';
+
+type Props = { children: string };
+
+type Story = StoryObj<Props>;
+type Meta = MetaObj<Props>;
+
+export const Default: Story = {
+  args: {
+    children: `\`\`\`mermaid
+graph LR
+A[Client] --> B(Load Balancer)
+B --> C{Server 1}
+B --> D{Server 2}
+\`\`\``,
+  },
+};
+
+export default {
+  title: 'Design System/Mermaid',
+  render: (_, { loaded: { Content } }) => Content,
+  loaders: [
+    async ({ args }) => {
+      const { content } = await compile(new VFile(args.children), 'mdx', {});
+
+      return { Content: content };
+    },
+  ],
+} as Meta;
